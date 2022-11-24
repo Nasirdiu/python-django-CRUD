189f83c9c9d7d7e964beaad54351c94c35ad7456
nasirdiu <nasir.joypurhat@gmail.com>
2022-11-24 12:14:15 +0600

    add data

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from myapp.models import Employees
 # Create your views here.
+
+def INDEX(request):
+    emp=Employees.objects.all()
+
+    context={
+        'emp':emp,
+    }
+    return render(request,'index.html',context)
+
+def ADD(request):
+    if request.method =="POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        address = request.POST.get('address')
+        phone = request.POST.get('phone')
+
+        emp = Employees(
+            name=name,
+            email=email,
+            address=address,
+            phone=phone
+        )
+        emp.save()
+        return redirect('home')
+    return render(request,'index.html')
+
